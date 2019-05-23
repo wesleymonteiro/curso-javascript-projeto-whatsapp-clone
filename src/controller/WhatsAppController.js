2eb608a0a9f1d6791e3cc61364a6f8e54648439d
Wesley Monteiro <wesley.monteiro@cadena.com.br>
2019-05-23 14:55:42 -0300

    move prototypes to another file and adds more functions

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,7 +1,8 @@
 class WhatsAppController {
   constructor() {
+    Prototype.initElementPrototypes()
     this.loadElements()
-    this.elementsPrototype()
+    this.initEvents()
   }
 
   loadElements() {
@@ -12,41 +13,47 @@ class WhatsAppController {
     })
   }
 
-  elementsPrototype() {
-    Element.prototype.hide = function() {
-      this.style.display = 'none'
-      return this
-    }
-    Element.prototype.show = function() {
-      this.style.display = 'block'
-      return this
-    }
-    Element.prototype.on = function(events, fn) {
-      events.split(' ').forEach(event=>{
-        this.addEventListener('event', fn)
-      })
-      return this
-    }
-    Element.prototype.css = function(styles) {
-      for(let name in styles) {
-        this.style[name] = styles[name]
+  initEvents() {
+    this.elements.myPhoto.on('click', e=>{
+      this.closeAllLeftPanel()
+      this.elements.panelEditProfile.show()
+      setTimeout(()=>{
+        this.elements.panelEditProfile.addClass('open')
+      }, 100)
+    })
+    this.elements.btnNewContact.on('click', e=>{
+      this.closeAllLeftPanel()
+      this.elements.panelAddContact.show()
+      setTimeout(()=>{
+        this.elements.panelAddContact.addClass('open')
+      }, 100)
+    })
+    this.elements.btnClosePanelEditProfile.on('click', e=>{
+      this.elements.panelEditProfile.removeClass('open')
+    })
+    this.elements.btnClosePanelAddContact.on('click', e=>{
+      this.elements.panelAddContact.removeClass('open')
+    })
+    this.elements.photoContainerEditProfile.on('click', e=>{
+      this.elements.inputProfilePhoto.click()
+    })
+    this.elements.inputNamePanelEditProfile.on('keypress', e=>{
+      if(e.key === 'Enter') {
+        e.preventDefault()
+        this.elements.btnSavePanelEditProfile.click()
       }
-      return this
-    }
-    Element.prototype.addClass = function(name) {
-      this.classList.add(name)
-      return this
-    }
-    Element.prototype.removeClass = function(name) {
-      this.classList.remove(name)
-      return this
-    }
-    Element.prototype.toggleClass = function(name) {
-      this.classList.toggle(name)
-      return this
-    }
-    Element.prototype.hasClass = function(name) {
-      return this.classList.contains(name)
-    }
+    })
+    this.elements.btnSavePanelEditProfile.on('click', e=>{
+      
+    })
+    this.elements.formPanelAddContact.on('submit', e=>{
+      e.preventDefault()
+      let formData = this.elements.formPanelAddContact.getForm()
+    })
+  }
+
+  closeAllLeftPanel() {
+    this.elements.panelAddContact.hide()
+    this.elements.panelEditProfile.hide()
   }
 }
