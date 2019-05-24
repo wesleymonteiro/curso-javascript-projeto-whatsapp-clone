238efef4357c3c2d1b2ae14e254176ec8a000de8
Wesley Monteiro <wesley.monteiro@cadena.com.br>
2019-05-24 11:18:18 -0300

    adds listeners to all attach menu buttons

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -50,6 +50,68 @@ class WhatsAppController {
       e.preventDefault()
       let formData = this.elements.formPanelAddContact.getForm()
     })
+    this.elements.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
+      item.on('click', e=>{
+        this.elements.home.hide()
+        this.elements.main.css({display: 'flex'})
+      })
+    })
+    this.elements.btnAttach.on('click', e=>{
+      e.stopPropagation()
+      this.elements.menuAttach.addClass('Open')
+      document.addEventListener('click', this.closeMenuAttach.bind(this))
+
+    })
+    this.elements.btnAttachPhoto.on('click', e=>{
+      this.elements.inputPhoto.click()
+    })
+    this.elements.inputPhoto.on('change', e=>{
+      [...this.elements.inputPhoto.files].forEach(file=>{
+        console.log(file)
+      })
+    })
+    this.elements.btnAttachCamera.on('click', e=>{
+      this.closeAllMainPanel()
+      this.elements.panelCamera.addClass('open')
+      this.elements.panelCamera.css({height: "calc(100% - 120px)"})
+      
+    })
+    this.elements.btnAttachDocument.on('click', e=>{
+      this.closeAllMainPanel()
+      this.elements.panelDocumentPreview.addClass('open')
+      this.elements.panelDocumentPreview.css({height: "calc(100% - 120px)"})
+    })
+    this.elements.btnAttachContact.on('click', e=>{
+      this.elements.modalContacts.show()
+    })
+    this.elements.btnClosePanelCamera.on('click', e=>{
+      this.elements.panelCamera.removeClass('open')
+      this.elements.panelMessagesContainer.show()
+    })
+    this.elements.btnClosePanelDocumentPreview.on('click', e=>{
+      this.elements.panelDocumentPreview.removeClass('open')
+      this.elements.panelMessagesContainer.show()
+    })
+    this.elements.btnCloseModalContacts.on('click', e=>{
+      this.elements.modalContacts.hide()
+    })
+    this.elements.btnTakePicture.on('click', e=>{
+
+    })
+    this.elements.btnSendDocument.on('click', e=>{
+
+    })
+  }
+
+  closeMenuAttach() {
+    document.removeEventListener('click', this.closeMenuAttach)
+    this.elements.menuAttach.removeClass('Open')
+  }
+
+  closeAllMainPanel() {
+    this.elements.panelMessagesContainer.hide()
+    this.elements.panelDocumentPreview.removeClass('open')
+    this.elements.panelCamera.removeClass('open')
   }
 
   closeAllLeftPanel() {
